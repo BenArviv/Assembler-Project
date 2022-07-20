626ec1ef626453da5c39149ce1d8a82ccaf8c83a
Ben Arviv <71941479+BenArviv@users.noreply.github.com>
2022-07-20 13:09:01 +0300

    added macros, updated enums

--- utils.h
+++ utils.h
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
+
+#define MAX_EXTENSION_LEN 5
+#define ERROR 1
 
 /* prototypes: */
 char *decToBase32(int);
 char *binToBase32(int);
 int binToDec(int);
 void decTo32(int, char[], char *);
+char *createFileName(char *, int);
 
 /* this array represents the symbols for our base-32 numbers */
 char key[32] = {'!', '@', '#', '$', '%', '^', '&', '*', '<', '>',
                      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'};
+
+enum suffix {FILE_INPUT, FILE_MACRO, FILE_OBJECT, FILE_ENTRY, FILE_EXTERN};
