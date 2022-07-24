9b56ccf131cbf935257cd5ad28575d57f4922dac
Ben Arviv <71941479+BenArviv@users.noreply.github.com>
2022-07-24 19:23:23 +0300

    added #ifndef

--- utils.h
+++ utils.h
@@ -3,8 +3,11 @@
 #include <string.h>
 #include <math.h>
 
-#define MAX_EXTENSION_LEN 5
-#define ERROR 1
+#ifndef UTILS.H
+
+#define UTILS.H
+
+#define MAX_EXTENSION_LEN 5 /* for file prefix */
 
 /* prototypes: */
 char *decToBase32(int);
@@ -14,8 +17,13 @@ void decTo32(int, char[], char *);
 char *createFileName(char *, int);
 
 /* this array represents the symbols for our base-32 numbers */
-char key[32] = {'!', '@', '#', '$', '%', '^', '&', '*', '<', '>',
+const char key[32] = {'!', '@', '#', '$', '%', '^', '&', '*', '<', '>',
                      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'};
 
 enum suffix {FILE_INPUT, FILE_MACRO, FILE_OBJECT, FILE_ENTRY, FILE_EXTERN};
+
+typedef enum {FALSE, TRUE} boolean; /* definition of boolean type */
+
+
+#endif
