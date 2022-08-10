5900ff646ace25e4f18a9bb6ed7f188f2c8600de
Ben Arviv <71941479+BenArviv@users.noreply.github.com>
2022-08-10 15:00:49 +0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "preAssembler.h"
 #include "firstPass2.h"
 #include "main.h"
 #include "structures.h"
+#include "labelUtils.h"
 
 int main(int argc, char *argv[])
 {
     int i;
     char *filename;
     char *amFilename;
+    FILE *fp;
+    MacroNode *head = NULL;
+    stringStruct cmdInit[] = {{"mov"},
+                              {"cmp"},
+                              {"add"},
+                              {"sub"},
+                              {"not"},
+                              {"clr"},
+                              {"lea"},
+                              {"inc"},
+                              {"dec"},
+                              {"jmp"},
+                              {"bne"},
+                              {"get"},
+                              {"prn"},
+                              {"jsr"},
+                              {"rts"},
+                              {"hlt"}};
+    stringStruct dataInit[] = {{".data"},
+                               {".string"},
+                               {".struct"},
+                               {".entry"},
+                               {".extern"}};
 
     extVars *vars = (extVars *)malloc(sizeof(extVars));
     vars -> ic = 0;
@@ -17,39 +42,9 @@ int main(int argc, char *argv[])
     vars -> externFlag = FALSE;
     vars -> recordedError = FALSE;
     vars -> symbolsTable = NULL;
-    vars -> cmd[] = {{"mov"},
-                    {"cmp"},
-                    {"add"},
-                    {"sub"},
-                    {"not"},
-                    {"clr"},
-                    {"lea"},
-                    {"inc"},
-                    {"dec"},
-                    {"jmp"},
-                    {"bne"},
-                    {"get"},
-                    {"prn"},
-                    {"jsr"},
-                    {"rts"},
-                    {"hlt"}};
-    vars -> dir[] = {{".data"},
-                    {".string"},
-                    {".struct"},
-                    {".entry"},
-                    {".extern"}};
-    /*
-    int dc = 0;
-    int ic = 0;
-    unsigned int data[MACHINE_RAM];
-    unsigned int instructions[MACHINE_RAM];
-    labelPtr symbolsTable = NULL;
-    boolean externFlag = FALSE;
-    boolean entryFlag = FALSE;
-    boolean recordedError = FALSE;
-    */
-    FILE *fp;
-    MacroNode *head = NULL;
+    vars -> cmd = cmdInit;
+    vars -> dir = dataInit;
+
     for (i = 1; i < argc; i++)
     {
         filename = createFileName(argv[i], FILE_INPUT);
@@ -60,8 +55,7 @@ int main(int argc, char *argv[])
             if (preAssembler(fp, amFilename, &head))
             {
                 fp = fopen(amFilename, "r");
-                firstPass2(fp, cmd, dir, vars);
-                /*firstPass(fp, cmd, dir, &externFlag, &symbolsTable, &dc, &ic, data, instructions, &recordedError); */
+                firstPass2(fp, vars); */
                 fclose(fp);
             }
             free(amFilename); /* we have a pointer, hence no longer needed */
