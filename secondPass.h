0ee204e8faf19dfb8bfcd00fd9a52cf72f1314f4
Ben Arviv <71941479+BenArviv@users.noreply.github.com>
2022-08-15 13:36:50 +0300

    added printCounters routine for fixed counters print

--- secondPass.h
+++ secondPass.h
@@ -12,20 +12,20 @@
 
 void secondPass(FILE *fp, char *filename, extVars *vars);
 void readLineSecond(char *line, extVars *vars);
-int handleCMDSecond(int cmdType, char *line, extVars *vars);
-int encodeAddtional(char *src, char *dest, boolean isSrc, boolean isDest, int srcMethod, int destMethod, extVars *vars);
-unsigned int registerOperand(boolean isDest, char *reg);
+void printCounters(FILE *fp, char *firstCol, char *secondCol);
 void additionalWords(boolean isDest, int method, char *op, extVars *vars);
 void encodeLabel(char *name, extVars *vars);
 void cmdOpernadsSecond(int type, boolean *isSrc, boolean *isDest);
-extPtr addExtern(extPtr *head, char *name, unsigned int ref);
 void freeExterns(extPtr *head);
-
-boolean writeOutput(char *filename, extVars *vars);
-FILE *openFile(char *filename, int type, extVars *vars);
 void writeOB(FILE *fp, extVars *vars);
 void writeExtern(FILE *fp, extVars *vars);
 void writeEntry(FILE *fp, extVars *vars);
+int handleCMDSecond(int cmdType, char *line, extVars *vars);
+int encodeAddtional(char *src, char *dest, boolean isSrc, boolean isDest, int srcMethod, int destMethod, extVars *vars);
+unsigned int registerOperand(boolean isDest, char *reg);
+extPtr addExtern(extPtr *head, char *name, unsigned int ref);
+boolean writeOutput(char *filename, extVars *vars);
+FILE *openFile(char *filename, int type, extVars *vars);
 
 
 
