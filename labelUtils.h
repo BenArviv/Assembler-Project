58072b5163baba03626e8d6ec06f20919a37b91d
Ben Arviv <71941479+BenArviv@users.noreply.github.com>
2022-08-15 13:29:49 +0300

    Update labelUtils.h

--- labelUtils.h
+++ labelUtils.h
@@ -5,17 +5,16 @@
 #include "firstPass2.h"
 
 /* PROTOTYPES */
-boolean isLabel(char *word, int colon, extVars *vars);
-labelPtr addLabel(char *name, unsigned int address, extVars *vars, boolean external);
-boolean isExistingLabel(labelPtr head, char *name);
-labelPtr getLabel(labelPtr head, char *name);
+void freeLabels(labelPtr *head);
+void offsetAdd(labelPtr label, int num, boolean isData);
 unsigned int getLabelAddress(labelPtr head, char *name);
+boolean isExistingLabel(labelPtr head, char *name);
 boolean isLabelExternal(labelPtr head, char *name);
 boolean entryLabel(labelPtr head, char *name, extVars *vars);
-void freeLabels(labelPtr *head);
+boolean isLabel(char *word, int colon, extVars *vars);
 boolean deleteLabel(labelPtr *head, char *name);
-void offsetAdd(labelPtr label, int num, boolean isData);
-
+labelPtr getLabel(labelPtr head, char *name);
+labelPtr addLabel(char *name, unsigned int address, extVars *vars, boolean external);
 
 void print_labels(labelPtr h);
 
